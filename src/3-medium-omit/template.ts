cd6388c5db13f76e95d4152a9c4c5f57b3e43a2b
benson <gwanbit@gmail.com>
2023-07-25 11:54:36 +0800

    feat: pick

--- src/3-medium-omit/template.ts
+++ src/3-medium-omit/template.ts
@@ -1,13 +1,3 @@
 type MyOmit<T, K extends keyof T> = {
   [P in keyof T as P extends K ? never : P]: T[P]
 }
-
-interface Todo {
-  title: string
-  description: string
-  completed: boolean
-}
-
-type TypeName = {
-  [P in keyof Todo]?: Todo[P]
-}
