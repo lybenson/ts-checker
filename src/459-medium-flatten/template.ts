8b6d48bf6d4db25c82f4c58ebbe84003ee081432
lybenson <naivebenson@gmail.com>
2023-08-20 12:07:33 +0800

    feat: 459

---
+++ src/459-medium-flatten/template.ts
@@ -0,0 +1,5 @@
+type Flatten<T extends unknown[]> = T extends [infer First, ...infer Rest]
+  ? First extends unknown[]
+    ? [...Flatten<First>, ...Flatten<Rest>]
+    : [First, ...Flatten<Rest>]
+  : []
