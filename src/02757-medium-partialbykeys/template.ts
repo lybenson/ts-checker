26b1f92d0927f7e8c727e178452c063545b48d4c
lybenson <gwanbit@gmail.com>
2023-08-26 17:25:23 +0800

    feat: 2757

--- src/02757-medium-partialbykeys/template.ts
+++
@@ -1,1 +0,0 @@
-type PartialByKeys<T, K> = any
---
+++ src/2757-medium-partialbykeys/template.ts
@@ -0,0 +1,9 @@
+type PartialByKeys<T, K extends keyof T = keyof T> = {
+  [P in keyof T as P extends K ? P : never]?: T[P]
+} & {
+  [P in keyof T as P extends K ? never : P]: T[P]
+} extends infer R
+  ? {
+      [P in keyof R]: R[P]
+    }
+  : never
