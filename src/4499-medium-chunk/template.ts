97228d8636d7fda0915fbb9950fba81a067aaa2d
lybenson <gwanbit@gmail.com>
2024-07-09 16:44:55 +0800

    feat: 4499

--- src/4499-medium-chunk/template.ts
+++ src/4499-medium-chunk/template.ts
@@ -1,1 +1,11 @@
-type Chunk = any
+type Chunk<
+  T extends any[],
+  N extends number = 1,
+  C extends any[] = []
+> = T extends [infer F, ...infer R]
+  ? C['length'] extends N
+    ? [C, ...Chunk<T, N>]
+    : Chunk<R, N, [...C, F]>
+  : C extends []
+  ? C
+  : [C]
--- src/4499-medium-chunk/test-cases.ts
+++ src/4499-medium-chunk/test-cases.ts
@@ -8,3 +8,5 @@ type cases = [
   Expect<Equal<Chunk<[1, 2, 3, 4], 5>, [[1, 2, 3, 4]]>>,
   Expect<Equal<Chunk<[1, true, 2, false], 2>, [[1, true], [2, false]]>>
 ]
+
+type C = Chunk<[1, true, 2, false], 2>
